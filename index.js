0dab07e41ea50780648d60f7d8be1e3fffd5db5f
danial <danialmalik321@gmail.com>
2019-06-01 10:31:22 +0500

    change port settings

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express()
 
 const controller = require('./controller');
 
-console.log(process.env.NODE_ENV, process.env);
-const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000
+const port = process.env.PORT || 3000;
 app.get('/', controller.get)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
